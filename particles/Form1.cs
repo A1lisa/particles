ea591207b94552b55af1db9cd565d20e67e5b309
A1lisa <aleksandra071078@icloud.com>
2026-05-12 13:56:51 +0800

    выполнена 1 часть

--- particles/Form1.cs
+++ particles/Form1.cs
@@ -12,84 +12,54 @@ namespace particles
 {
     public partial class Form1 : Form
     {
-        List<Particle> particles = new List<Particle>();
+       Emitter emitter = new Emitter();
 
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image=new Bitmap (picDisplay.Width,picDisplay.Height);
-        }
-
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
+            
+            emitter = new TopEmitter
             {
-                particle.Life -= 1;
-                if (particle.Life < 0)
-                {
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
+                Width = picDisplay.Width,
+                GravitationY = 0.25f
+            };
 
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
+             emitter.impactPoints.Add(new GravityPoint
+             {
+                 X = (float)(picDisplay.Width * 0.25),
+                 Y = picDisplay.Height / 2
+             });
 
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+             emitter.impactPoints.Add(new AntiGravityPoint
+             {
+                 X = picDisplay.Width / 2,
+                 Y = picDisplay.Height / 2
+             });
 
-                    particle.Radius = 2 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                }
-            }
-
-            for (var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 500)
-                {
-                    var particle = new ParticleColorful();
-                    particle.FromColor = Color.Yellow;
-                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break; 
-                }
-            }
-        }
-        private void Render(Graphics g)
-        {
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
+             emitter.impactPoints.Add(new GravityPoint
+             {
+                 X = (float)(picDisplay.Width * 0.75),
+                 Y = picDisplay.Height / 2
+             });
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+            emitter.UpdateState();
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
                 g.Clear(Color.Black);
-                Render(g);
+                emitter.Render(g);
             }
             picDisplay.Invalidate();
 
         }
-
-        private int MousePositionX = 0;
-        private int MousePositionY = 0;
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            MousePositionX = e.X;
-            MousePositionY = e.Y;
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
